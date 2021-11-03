2e562e0d2f94770bf8dfc82bd6f867ba9fef2af6
bak96 <piotrbak1996@gmail.com>
2021-11-03 23:02:04 +0100

    simple level'

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -17,7 +17,7 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + dist, cam.transform.position.y * parallaxEffect, transform.position.z);
 
         if (temp > startpos + length) {
             startpos += length;
